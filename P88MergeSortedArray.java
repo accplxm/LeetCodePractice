ea0724c71ccc7abe35d9405c424ddd6e844e377b
MengKai <214697893@qq.com>
2019-11-17 11:13:00 +0800

    leetcode 88

--- P88MergeSortedArray.java
+++ P88MergeSortedArray.java
@@ -47,13 +47,10 @@ public class P88MergeSortedArray {
             n--;
             while (m >= 0 && n >= 0) {
                 if (nums1[m] > nums2[n]) {
-                    nums1[i] = nums1[m];
-                    m--;
+                    nums1[i--] = nums1[m--];
                 } else {
-                    nums1[i] = nums2[n];
-                    n--;
+                    nums1[i--] = nums2[n--];
                 }
-                i--;
             }
             while (n >= 0) {
                 nums1[i--] = nums2[n--];
