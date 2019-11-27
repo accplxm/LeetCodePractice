61047cb9c4b939ad8f67e2a7b0e028b919f5a75c
MengKai <214697893@qq.com>
2019-11-27 09:50:26 +0800

    leetcode 17

--- P17LetterCombinationsOfAPhoneNumber.java
+++ P17LetterCombinationsOfAPhoneNumber.java
@@ -18,21 +18,26 @@ package leetcode.editor.cn;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 //java:电话号码的字母组合
 public class P17LetterCombinationsOfAPhoneNumber {
     public static void main(String[] args) {
         Solution solution = new P17LetterCombinationsOfAPhoneNumber().new Solution();
+
         //TO TEST
-        solution.letterCombinations("23");
+        //1、递归回溯
+        System.out.println(solution.letterCombinations("5678"));
+
+
     }
 
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
-        HashMap<String, String> map = new HashMap<String, String>() {{
+        HashMap<String, String> mapPhone = new HashMap<String, String>() {{
             put("2", "abc");
             put("3", "def");
             put("4", "ghi");
@@ -45,14 +50,44 @@ public class P17LetterCombinationsOfAPhoneNumber {
         List<String> ouput = new ArrayList<>();
 
         public List<String> letterCombinations(String digits) {
+            //递归
+//            if (digits.length() != 0){
+//                strCombination("", digits);
+//            }
+//            return ouput;
 
+            //队列，迭代
+            LinkedList<String> linkedList = new LinkedList<>();
+            if (digits.length() > 0) {
+                linkedList.add("");
+                String[] digitMapLetter = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+                while (linkedList.peek().length() != digits.length()) {
+                    String popStr = linkedList.remove();
+                    String letter = digitMapLetter[digits.charAt(popStr.length()) - '0'];
+                    for (char ch : letter.toCharArray()) {
+                        linkedList.addLast(popStr + ch);
+                    }
 
-            for (int i = 0; i < digits.length(); i++) {
-                String str = digits.substring(i, i + 1);
-                System.out.println(str);
+                }
             }
-            return null;
+            return linkedList;
+
         }
+
+        public void strCombination(String combination, String nextStr) {
+            if (nextStr.length() == 0) {
+                ouput.add(combination);
+            } else {
+                String strDigit = nextStr.substring(0, 1);
+                String letters = mapPhone.get(strDigit);
+                for (int i = 0; i < letters.length(); i++) {
+                    String letter = letters.substring(i, i + 1);
+                    strCombination(combination + letter, nextStr.substring(1));
+                }
+
+            }
+        }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
